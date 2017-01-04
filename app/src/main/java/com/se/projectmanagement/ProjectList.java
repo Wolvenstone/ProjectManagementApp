8a5b6cd06c2e51689c345231e5b5b5265c41b711
romansum <r.sumereder@gmx.at>
2017-01-04 19:40:09 +0100

    Added DB-Connection for the projects

--- app/src/main/java/com/se/projectmanagement/ProjectList.java
+++ app/src/main/java/com/se/projectmanagement/ProjectList.java
@@ -6,6 +6,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,6 +35,55 @@ public class ProjectList extends ListActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        try {
+            URL url = new URL("http://10.0.2.2:7777/api/projects");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+
+            InputStream in = new BufferedInputStream(conn.getInputStream());
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            StringBuilder total = new StringBuilder(in.available());
+            String line;
+            while ((line = reader.readLine()) != null) {
+                total.append(line).append('\n');
+            }
+            System.out.println(total.toString());
+            JSONArray json=null;
+            try {
+                json = new JSONArray(total.toString());
+                listItems = new ArrayList<Project>();
+                if (json != null) {
+                    for (int i = 0; i < json.length(); i++) {
+                        JSONObject bb = (JSONObject) json.get(i);
+                        String id = String.valueOf(bb.get("_id"));
+                        String title = String.valueOf(bb.get("title"));
+                        String description = String.valueOf(bb.get("description"));
+                        //System.out.println(title);
+                        Project p = new Project (id,title,description);
+                        listItems.add(p);
+
+                    }
+                }
+
+                System.out.println(listItems);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            conn.disconnect();
+
+        } catch (MalformedURLException e) {
+
+            e.printStackTrace();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+
         setContentView(R.layout.project);
         adapter=new ArrayAdapter<Project>(this,
                 android.R.layout.simple_list_item_1,
